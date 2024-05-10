414495ac5789bdcd0c7d6d91f14060ab425c3e98
fuadmammadov33 <fuaddybala3@gmail.com>
2024-05-10 13:38:39 +0400

    first commit

---
+++ Kamil's MultiShop/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.DataAccesLayer;
+using WebApplication1.ViewModels.CategoriesVm;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+
+        private readonly MultiShopContext _context;
+
+        public HomeController(MultiShopContext context)
+        {
+            _context = context;
+        }
+        
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Categories.Where(x => x.IsDeleted == false)
+                .Select(x => new GetCategoryVM
+                {
+                    Id = x.Id,
+                    Name = x.Name
+                })
+                .ToListAsync();
+            return View(data);
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Kamil's MultiShop/WebApplication1/DataAccesLayer/MultiShopContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+using WebApplication1.Areas.Admin.Models;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.DataAccesLayer
+{
+    public class MultiShopContext : DbContext
+    {
+        public MultiShopContext(DbContextOptions options) : base(options)
+        {
+        }
+    
+        public DbSet<Category> Categories { get; set; }
+     
+        public DbSet<Sliders> Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("CA-R214-PC19\\SQLEXPRESS;Database=MultiShop;Trusted_Connection=true;TrustServerCertificate=True");
+            base.OnConfiguring(options);
+        }
+    }
+}
